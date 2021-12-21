29701a78f617960b12a0ed912642fb5bef26b619
Areg Vardanian <aregv@bk.ru>
2021-12-21 14:14:07 +0300

    Initial Commit

---
+++ Tinder/TinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderApp.swift
+//  Tinder
+//
+//  Created by Геворг on 21.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct TinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
